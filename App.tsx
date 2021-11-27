1cec0738064ff3da53f04c01a18201f896d4c3a1
tan-pham-97 <tan.pham@infodation.vn>
2021-11-27 21:39:45 +0700

    first code

--- App.tsx
+++ App.tsx
@@ -1,14 +1,56 @@
-import React from "react";
-import { SafeAreaView, ScrollView, Text, View } from "react-native";
+import React, { useState, useEffect } from "react";
+import {
+  SafeAreaView,
+  ScrollView,
+  Text,
+  View,
+  TouchableOpacity,
+} from "react-native";
 import { FONTS } from "./src/constants";
 
 const App = () => {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {});
+
+  function themMotDonVi() {
+    setCount(count + 1);
+  }
+
+  function giamMotDonVi() {
+    setCount(count - 1);
+  }
+
   return (
     <SafeAreaView>
       <ScrollView contentInsetAdjustmentBehavior="automatic">
-        <View>
-          <Text style={{ ...FONTS.h3 }}>This is font Roboto Bold</Text>
+        <TouchableOpacity onPress={() => giamMotDonVi()}>
+          <View
+            style={{
+              flex: 1,
+              backgroundColor: "red",
+              alignItems: "center",
+              padding: 10,
+            }}
+          >
+            <Text>Tru</Text>
+          </View>
+        </TouchableOpacity>
+        <View style={{ alignItems: "center", padding: 75 }}>
+          <Text style={{ ...FONTS.h3 }}>{count}</Text>
         </View>
+        <TouchableOpacity onPress={() => themMotDonVi()}>
+          <View
+            style={{
+              flex: 1,
+              backgroundColor: "red",
+              alignItems: "center",
+              padding: 10,
+            }}
+          >
+            <Text>Cong</Text>
+          </View>
+        </TouchableOpacity>
       </ScrollView>
     </SafeAreaView>
   );
